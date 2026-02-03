38efd06aec40b592a7bda2c9bf98fd9087777eea
Constantin Koch <koch.constantin@gmx.net>
2026-02-03 15:30:28 +0100

    Initial commit

---
+++ src/client/java/cokonut19/litematicportal/LitematicPortalClient.java
@@ -0,0 +1,10 @@
+package cokonut19.litematicportal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LitematicPortalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
